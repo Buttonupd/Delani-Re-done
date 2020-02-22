25bfd591b30ba14182a634602a2cea42eed8ab97
Buttonupd <Dankariuki0101@gmail.com>
2020-02-22 09:47:21 +0300

    partial html layout

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
+function  math(num1,num2){
+    var added = num1 + num2;
+   
+    console.log(added);
+     return added;
+}
+added(10,20)
+
 $(document).ready(function() {
     $("p").click(function() {
       $(".img").toggle()});
     });
+
+$(document).ready(function(){
+    $(".floated").click(function(){
+        $("img").toggle()
+        $("p").click()
+        alert("hel")
+    })
+})
