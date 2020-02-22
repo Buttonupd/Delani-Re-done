391ec16a285b60ca54f929b5336680599cf48c56
Buttonupd <Dankariuki0101@gmail.com>
2020-02-22 14:08:03 +0300

    working on forms to receive and send out confirmations

--- js/index.js
+++ js/index.js
@@ -34,8 +34,74 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $("#one").mouseover(function(){
-        $("#overlay1").show();
+        $("#switch_board").show();
     }).mouseout(function(){
-        $("#overlay1").hide();
+        $("#switch_board").hide();
     });
 });
+
+$(document).ready(function(){
+    $("#two").mouseover(function(){
+        $("#switch_board2").show();
+    }).mouseout(function(){
+        $("#switch_board2").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#three").mouseover(function(){
+        $("#switch_board3").show();
+    }).mouseout(function(){
+        $("#switch_board3").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#four").mouseover(function(){
+        $("#switch_board4").show();
+    }).mouseout(function(){
+        $("#switch_board4").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#five").mouseover(function(){
+        $("#switch_board5").show();
+    }).mouseout(function(){
+        $("#switch_board5").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#six").mouseover(function(){
+        $("#switch_board6").show();
+    }).mouseout(function(){
+        $("#switch_board6").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#seven").mouseover(function(){
+        $("#switch_board7").show();
+    }).mouseout(function(){
+        $("#switch_board7").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#eight").mouseover(function(){
+        $("#switch_board8").show();
+    }).mouseout(function(){
+        $("#switch_board8").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#form").submit(function(event){
+        event.preventDefault();
+        var name = $("input#merge1").val();
+        var email = $("input#merge0").val();
+        var message = $("textarea#comment").val();
+        
+    })
+})
