3bae4c63d80a89f56d3dfbeff9535dcfd28b7afa
Buttonupd <Dankariuki0101@gmail.com>
2020-02-22 12:55:42 +0300

    Design and DEvelopment jquery effects properly working

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
     $("#dev-image").click(function(){
         $("#dev-image").slideDown('1500').hide('1000');
-        $("#dev-image").show('1500');
+        $("#DEVELOPMENT").show('1500');
     });
     $("#DEVELOPMENT").click(function(){
-        $("#DEVELOPMENT").slideup('1500');
+        $("#DEVELOPMENT").slideUp('1500');
         $("#dev-image").slideDown('1500');
     })
 });
 
 $(document).ready(function(){
-    $("#fashion-img").click(function(){
-        $("#fashion-img").slideDown('1500').hide('1000');
+    $("#Fashion-img").click(function(){
+        $("#Fashion-img").slideDown('1500').hide('1000');
         $("#DESIGN").show('1500');
     })
     $("#DESIGN").click(function(){
         $("#DESIGN").slideUp('1500');
-        
-    })
-})
+        $("#Fashion-img").slideDown('1500');
+
+    });
+});
